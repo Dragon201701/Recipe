e1c3da64393dbda26e6c640efea2b40b25f2f005
Dragon201701 <yz-liu@live.cn>
2019-02-04 17:13:37 -0600

    Create Roast Pork.mk

---
+++ Roast Pork.mk
@@ -0,0 +1,18 @@
+# Chinese Roast Pork (红烧肉，hong shao rou)
+---
+This recipie introduce how to cook roast pork, one of the traditional chinese cusine. Roast Pork can be done in as quick as 30 minutes by adding coke or longer than 2 hours without coke.
+---
+## Ingredient
+
+  + Pork Belly 五花肉
+  + Light Soy Sause 生抽 
+  + Dark Soy Sause 老抽
+  + Crystal Sugar 冰糖
+  + Vegetable/Peanut Oil
+  + Ginger 姜
+  + Green Onion 大葱
+  + Five Spice Powder 五香粉
+  + Cooking Wine/Rice wine 料酒
+---
+
+
